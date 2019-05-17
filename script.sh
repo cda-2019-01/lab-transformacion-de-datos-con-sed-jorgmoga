5859e4b38e4de484d7f14ce1c3d17b484986cf37
Jorge Eliecer Montoya Gallo <50222297+jorgmoga@users.noreply.github.com>
2019-05-16 20:59:54 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 # Escriba su código aquí
-# Escriba su código aquí
 sed 's/,\([0-9]\)/.\1/' data.csv > out.11 #Rempazo , seguido de un número por . y el mismo numero
 sed 's/\//-/g' out.11 > out.12 #Remplazo / por -  Cascarita: para identificar el / hay que poner \/
 sed 's/-\([0-9][0-9]\);/-20\1;/' out.12 > out.13 #cambio el formato de YY a YYYY
@@ -11,7 +10,12 @@ sed 's/;;/;\\N;/g' out.17 > out.18 ##Completo los null \N
 sed 'y/abcn/ABCN/' out.18 > out.19 #Remplaza a por a por A
 sed 's/;N;/;\\N;/g' out.19 > out.20 ##Completo los N por \N
 sed 's/;\r/;\\N\r/g' out.20 > out.21 #agrego el null al final
+##sed 's/;\r/;\\N\r/g' out.20 > out.21 #agrego el null al final
 sed 's/;/,/g' out.21 > out.22 ##Remplazo los ; por ,
 sed 's/,N,/,\N,/g' out.22 > out.23 ##Completo los N por \N
 sed 's/,N\r/,\\N\r/g' out.23 > out.24 ##Completo los N por \N
-cat out.24
+##sed 's/,N\r/,\\N\r/g' out.23 > out.24 ##Completo los N por \N
+cp out.24 data1.csv #
+cat data1.csv 
+rm out*
+#cat -A data1.csv
